558e99b3c43aa7b58f46de0e87a407c6e3f60b37
Stefanija Duracoska <stefanija@hubbyesim.com>
2026-06-05 15:57:16 +0200

    chore: regenerate migrations for postgres

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20260605_132157 from './20260605_132157';
+import * as migration_20260605_135653 from './20260605_135653';
 
 export const migrations = [
   {
-    up: migration_20260605_132157.up,
-    down: migration_20260605_132157.down,
-    name: '20260605_132157'
+    up: migration_20260605_135653.up,
+    down: migration_20260605_135653.down,
+    name: '20260605_135653'
   },
 ];
